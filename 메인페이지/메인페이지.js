ec249baac3f97dcff21db17d2f5388c4e766c99b
Sialsry <https://github.com/Sialsry>
2025-02-12 21:43:36 +0900

    e

--- 메인페이지/메인페이지.js
+++ 메인페이지/메인페이지.js
@@ -3,12 +3,8 @@ document.querySelector('.draw').onclick =() => {
 }
 
 function viewImage(imageSrc) {
-    // 클릭한 이미지의 경로를 쿼리 스트링에 추가하여 이동
-    // window.location.href = "../그림감상페이지.html?image=" + encodeURIComponent(imageSrc);
-    window.location.href = `../그림감상페이지/그림감상페이지.html?image=` + imageSrc;
+    window.location.href = `../그림감상페이지/그림감상페이지.html?image=` + imageSrc; // 클릭한 이미지의 경로를 쿼리 스트링에 추가하여 이동
 }
-console.log(localStorage.getItem("images"))
-// console.log(encodeURIComponent(imageSrc))
 
 const arr = JSON.parse(localStorage.getItem("images")) || []
 
